9fd57d2626704f3e0a9db1683319a1cb40acf718
JoeQuigley1 <joeyjtq@hotmail.com>
2022-12-05 18:46:09 +0000

    Add initial templates and css

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic 
+from .models import Recipe
 
-# Create your views here.
+
+
+class RecipeList(generic.ListView):
+    model = Recipe
+
+    queryset = Recipe.objects.filter(status=1).order_by('created_on')
+    template_name = 'recipe_posts.html'
+    paginate_by = 6
