57cd6079ee1d4ca01ddb55400bf008c36b7fcf50
JoeQuigley1 <joeyjtq@hotmail.com>
2022-12-05 20:29:16 +0000

    connect urls and fix cloudinary error

--- cookbook/views.py
+++ cookbook/views.py
@@ -3,10 +3,12 @@ from django.views import generic
 from .models import Recipe
 
 
+class HomePage(generic.TemplateView):
+    template_name = 'index.html'
+
 
 class RecipeList(generic.ListView):
     model = Recipe
-
     queryset = Recipe.objects.filter(status=1).order_by('created_on')
     template_name = 'recipe_posts.html'
     paginate_by = 6
