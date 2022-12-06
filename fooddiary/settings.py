057d7124b8accf2c9e7cc8dea7b2bd7ae3a39e30
JoeQuigley1 <joeyjtq@hotmail.com>
2022-12-06 20:25:20 +0000

    Add comment form functionality and link recipe posts to the nav

--- fooddiary/settings.py
+++ fooddiary/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'django.contrib.staticfiles',
     'django_summernote',
+    'crispy_forms',
     'cookbook',
 ]
 
@@ -56,6 +57,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
